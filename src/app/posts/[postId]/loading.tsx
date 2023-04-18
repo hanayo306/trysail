af98451b5275468b5e2f23e1e620faf28077e2cb
kkukileon305 <kkukileon305@gmail.com>
2023-04-19 01:55:04 +0900

    small fix skeleton ui

--- src/app/posts/[postId]/loading.tsx
+++ src/app/posts/[postId]/loading.tsx
@@ -9,13 +9,10 @@ const Loading = () => {
         <Skeleton variant="rounded" width={100} height={32} />
         <Skeleton variant="rounded" width={90} height={44} />
       </div>
-      <Skeleton variant="text" width={50} height={24} className="mb-4" />
-      <Skeleton variant="text" width={100} height={24} className="mb-4" />
+      <Skeleton variant="text" width={50} height={24} className="mb-8" />
+      <Skeleton variant="text" width={100} height={24} className="mb-8" />
 
-      <div className="flex gap-4">
-        <Skeleton variant="rounded" width="50%" height={600} />
-        <Skeleton variant="rounded" width="50%" height={600} />
-      </div>
+      <Skeleton variant="rounded" width="100%" height={600} />
     </>
   );
 };
