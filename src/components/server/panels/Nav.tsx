cec7ec14a07aa176bb96b1397e5dc2a82a6e39ab
kkukileon305 <kkukileon305@gmail.com>
2023-04-19 01:54:28 +0900

    translate Nav to ASC and add user information, logout button, and responsive style

--- src/components/server/panels/Nav.tsx
+++ src/components/server/panels/Nav.tsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
+import { cookies } from "next/headers";
 import Link from "next/link";
 import { BsSearch } from "react-icons/bs";
+import getUserByToken from "@/utils/getUserByToken";
+import LogoutBtn from "@/components/client/buttons/LogoutBtn";
+import LoginBtn from "@/components/client/buttons/LoginBtn";
 
-const Nav = () => {
+const Nav = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("access_token")?.value;
+
+  const userInformation = await getUserByToken(token);
+
   return (
-    <nav className="border-b sticky top-0 bg-white/50 backdrop-blur">
-      <div className="container mx-auto py-2 flex justify-between items-center p-4">
+    <nav className="border-b sticky top-0 bg-white/50 backdrop-blur z-10">
+      {/* pc */}
+      <div className="container mx-auto hidden md:flex justify-between items-center px-4 py-2">
         <h1 className="font-bold text-2xl">
           <Link href={"/"}>Logo</Link>
         </h1>
@@ -13,6 +24,35 @@ const Nav = () => {
           <BsSearch />
         </button>
       </div>
+
+      {/* mobile */}
+      {userInformation && (
+        <div className="container mx-auto flex items-center justify-between md:hidden px-4 py-2 gap-2 relative">
+          <Image
+            src={userInformation.profile.profile_picture_url}
+            alt={`${userInformation.profile.user_name}의 프로필사진`}
+            width={36}
+            height={36}
+            className="rounded-full overflow-hidden"
+          />
+
+          <h1 className="font-bold text-2xl absolute left-1/2 -translate-x-1/2">
+            <Link href={"/"}>Logo</Link>
+          </h1>
+
+          <LogoutBtn />
+        </div>
+      )}
+
+      {!userInformation && (
+        <div className="container mx-auto flex items-center justify-end md:hidden px-4 py-2 gap-2 relative">
+          <h1 className="font-bold text-2xl absolute left-1/2 -translate-x-1/2">
+            <Link href={"/"}>Logo</Link>
+          </h1>
+
+          <LoginBtn />
+        </div>
+      )}
     </nav>
   );
 };
