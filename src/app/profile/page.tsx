99480e222871edf99a2d03fcdf188db0bc37f7d0
kkukileon305 <kkukileon305@gmail.com>
2023-04-22 09:52:57 +0900

    마이 페이지 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,38 @@
+import { cookies } from "next/headers";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 import LogoutBtn from "@/components/client/buttons/LogoutBtn";
+import getUserByToken from "@/utils/getUserByToken";
+import blurDataURL from "@/const";
 
 const Page = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("access_token")?.value;
+
+  const userInformation = await getUserByToken(token);
+
+  if (!userInformation) {
+    redirect("auth/login");
+  }
+
   return (
     <div className="py-4 px-2 md:px-4">
-      <h2 className="mb-8">My Page</h2>
-
-      <LogoutBtn />
+      <div className="mx-auto max-w-3xl">
+        <div className="flex justify-between items-start mb-8">
+          <Image
+            src={userInformation.profile.profile_picture_url}
+            placeholder="blur"
+            blurDataURL={blurDataURL}
+            width={120}
+            height={120}
+            className="overflow-hidden object-cover w-[120px] h-[120px] border rounded-full cursor-pointer hover:border-blue-400 transition-[0.3s]"
+            alt={userInformation.profile.user_name}
+          />
+          <LogoutBtn />
+        </div>
+        <h2 className="font-bold text-xl mb-2">{userInformation.profile.user_name}</h2>
+        <p>{userInformation.user.email}</p>
+      </div>
     </div>
   );
 };
