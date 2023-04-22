09607ddd16c6f89c19d9243e8139c08bc529b416
kkukileon305 <kkukileon305@gmail.com>
2023-04-22 19:35:25 +0900

    add force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Suspense } from "react";
 import MainSkeleton from "@/components/client/skeletons/MainSkeleton";
 import Main from "@/components/server/panels/Main";
 
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+
 const Page = async () => {
   return (
     <Suspense fallback={<MainSkeleton />}>
